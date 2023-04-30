9589454ccc0917c5129512e327640ebbc843af9e
ardit <kosinoff.jenia1226@gmail.com>
2023-04-30 13:53:16 +0300

    Day 26: Add header, total_price, company name and logo

--- main.py
+++ main.py
@@ -19,9 +19,8 @@ for filepath in filepaths:
 
     pdf.set_font(family='Times', size=16, style='B')
     pdf.cell(w=50, h=8, txt=f'Date: {date}', ln=1)
-
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    # Add a header
+# Add a header
     columns = df.columns
     columns = [item.replace('_', ' ').title() for item in columns]
     pdf.set_font(family='Times', size=10, style='B')
@@ -32,7 +31,7 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
-    # Add rows to the table
+# Add rows to the table
     for index, row in df.iterrows():
         pdf.set_font(family='Times', size=10)
         pdf.set_text_color(80, 80, 80)
@@ -41,5 +40,22 @@ for filepath in filepaths:
         pdf.cell(w=35, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family='Times', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=70, h=8, txt='', border=1)
+    pdf.cell(w=35, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt='', border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+# Add Total sum sentence
+    pdf.set_font(family='Times', size=10)
+    pdf.cell(w=30, h=8, txt=f'The total price is {total_sum}', ln=1)
+
+# Add company name and logo
+    pdf.set_font(family='Times', size=14)
+    pdf.cell(w=30, h=8, txt=f'My company')
+    pdf.image('other_files/pythonhow.png', w=7, h=7)
 
     pdf.output(f'PDFs/{filename}.pdf')
